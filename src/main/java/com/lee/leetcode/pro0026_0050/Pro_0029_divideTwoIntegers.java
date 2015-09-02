7820d3ec8e6769f12238cf97d2f7f45e2fbf1e8b
li.li.sh2 <li.li.sh2@dianping.com>
2015-09-02 18:41:23 +0800

    Problem 29, 30

--- src/main/java/com/lee/leetcode/pro0026_0050/Pro_0029_divideTwoIntegers.java
+++ src/main/java/com/lee/leetcode/pro0026_0050/Pro_0029_divideTwoIntegers.java
@@ -3,10 +3,65 @@ package com.lee.leetcode.pro0026_0050;
 public class Pro_0029_divideTwoIntegers {
 
 	public static void main(String[] args) {
-		
+		int dividend = -1375621307;
+		int divisor =  -1102871504;
+		System.out.println(dividend + divisor);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(dividend / divisor);
+		System.out.println(divide(dividend, divisor));
 	}
 
 	public static int divide(int dividend, int divisor) {
-        return 0;
+		if(divisor == 0) { return Integer.MAX_VALUE; }
+		if(dividend == 0) { return 0; }
+		if(divisor == -1) {
+			if(dividend == Integer.MIN_VALUE) { return Integer.MAX_VALUE; }
+			return -dividend;
+		}
+		if(divisor == 1) { return dividend; }
+		
+		long nDend, nDsor;
+		boolean isNegative = true;
+		if(dividend > 0) {
+			nDend = -dividend;
+			if(divisor > 0) {
+				nDsor = -divisor;
+				isNegative = false;
+			}else {
+				nDsor = divisor;
+			}
+		}else {
+			nDend = dividend;
+			if(divisor > 0) {
+				nDsor = -divisor;
+			}else {
+				nDsor = divisor;
+				isNegative = false;
+			}
+		}
+		
+		if(nDend > nDsor) { return 0; }
+		if(nDend == nDsor) { return isNegative ? -1 : 1; }
+		
+		long result = nDend - nDsor, d = nDsor;
+		int q = 0, factor = 1;
+		while(true) {
+			if(result == 0) {
+				q += factor;
+				break;
+			}else if(result < 0) {
+				q += factor;
+				nDend = result;
+				factor *= 2;
+				nDsor *= 2;
+			}else {
+				if(nDsor == d) { break; }
+				factor /= 2;
+				nDsor /= 2;
+			}
+			result = nDend - nDsor;
+		}
+        return isNegative ? -q : q;
     }
 }
--- src/main/java/com/lee/leetcode/pro0026_0050/Pro_0030_substringWithConcatenationOfAllWords.java
+++ src/main/java/com/lee/leetcode/pro0026_0050/Pro_0030_substringWithConcatenationOfAllWords.java
@@ -1,14 +1,85 @@
 package com.lee.leetcode.pro0026_0050;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Pro_0030_substringWithConcatenationOfAllWords {
 
 	public static void main(String[] args) {
-		
+		String s = "barfoothefoobarman";
+		String[] words = {"foo", "bar"};
+		System.out.println(findSubstring(s, words));
 	}
 
     public static List<Integer> findSubstring(String s, String[] words) {
-        return null;
+    	int sLen = s.length();
+        int count = words.length;
+        if(count == 0) { return Arrays.asList(0); }
+        int wLen = words[0].length();
+        if(wLen == 0) { return Arrays.asList(0); }
+        int wsLen = wLen*count;
+        if(wsLen > sLen) { return Collections.emptyList(); }
+        
+        if(count == 1) {
+        	List<Integer> list = new ArrayList<Integer>();
+        	int max = sLen - wLen;
+        	String w = words[0];
+        	for(int i=0; i<=max; i++) {
+        		boolean match = true;
+        		for(int j=0; j<wLen; j++) {
+        			if(s.charAt(i+j) != w.charAt(j)) {
+        				match = false;
+        				break;
+        			}
+        		}
+        		if(match) { list.add(i); }
+        	}
+        	return list;
+        }
+        
+        
+        List<Integer> list = new ArrayList<Integer>();
+        boolean[] flags = new boolean[count];
+        int max = sLen - wsLen;
+        for(int i=0; i<=max; i++) {
+        	boolean match = true;
+        	int matchCount = 0;
+        	int begin = i;
+        	while(matchCount < count) {
+        		if(matchOneWord(s, begin, words, matchCount, flags, wLen)) {
+        			matchCount++;
+        			begin += wLen;
+        		}else {
+        			match = false;
+        			break;
+        		}
+        	}
+        	if(match) { list.add(i); }
+        	Arrays.fill(flags, false);
+        }
+    	return list;
+    }
+    
+    private static final boolean matchOneWord(String s, int begin, String[] words, int matchCount, boolean[] flags, int wLen) {
+    	for(int i=0; i<words.length; i++) {
+    		if(!flags[i] && match(s, begin, words[i], wLen)) {
+    			flags[i] = true;
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    private static final boolean match(String s, int begin, String word, int wLen) {
+    	boolean match = true;
+    	for(int i=0; i<wLen; i++) {
+    		if(s.charAt(begin+i) != word.charAt(i)) {
+				match = false;
+				break;
+			}
+    	}
+    	return match;
     }
 }
