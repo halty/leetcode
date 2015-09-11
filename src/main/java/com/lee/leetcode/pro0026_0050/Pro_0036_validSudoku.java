ec42a509ec550a1dc3d4685e38ded80fa6aaf50a
li.li.sh2 <li.li.sh2@dianping.com>
2015-09-11 14:01:51 +0800

    add Problem 36-40

---
+++ src/main/java/com/lee/leetcode/pro0026_0050/Pro_0036_validSudoku.java
@@ -0,0 +1,49 @@
+package com.lee.leetcode.pro0026_0050;
+
+/*
+ * 
+Determine if a Sudoku is valid.
+
+Sudoku Puzzles - The Rules:
+	1. Each row must have the numbers 1-9 occuring just once.
+	2. Each column must have the numbers 1-9 occuring just once.
+	3. And the numbers 1-9 must occur just once in each of the 9 sub-boxes of the grid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+	=========================================
+	|| 5 | 3 |   ||   | 7 |   ||   |   |   ||
+	-----------------------------------------
+	|| 6 |   |   || 1 | 9 | 5 ||   |   |   ||
+	-----------------------------------------
+	||   | 9 | 8 ||   |   |   ||   | 6 |   ||
+	=========================================
+	|| 8 |   |   ||   | 6 |   ||   |   | 3 ||
+	-----------------------------------------
+	|| 4 |   |   || 8 |   | 3 ||   |   | 1 ||
+	-----------------------------------------
+	|| 7 |   |   ||   | 2 |   ||   |   | 6 ||
+	=========================================
+	||   | 6 |   ||   |   |   || 2 | 8 |   ||
+	-----------------------------------------
+	||   |   |   || 4 | 1 | 9 ||   |   | 5 ||
+	-----------------------------------------
+	||   |   |   ||   | 8 |   ||   | 7 | 9 ||
+	=========================================
+
+A partially filled sudoku which is valid.
+
+Note:
+A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+ * 
+ */
+public class Pro_0036_validSudoku {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static boolean isValidSudoku(char[][] board) {
+        return false;
+    }
+}
---
+++ src/main/java/com/lee/leetcode/pro0026_0050/Pro_0039_combinationSum.java
@@ -0,0 +1,30 @@
+package com.lee.leetcode.pro0026_0050;
+
+import java.util.List;
+
+/*
+ * 
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+ * 
+ */
+public class Pro_0039_combinationSum {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return null;
+    }
+}
