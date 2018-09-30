a2d7243e4de95820c3d4ab1c811c697c639ffc40
li.li.sh2 <li.li.sh2@dianping.com>
2018-09-30 17:59:34 +0800

    commit problem 53,54

---
+++ src/main/java/com/lee/leetcode/pro0051_0075/Pro_0053_MaximumSubarray.java
@@ -0,0 +1,108 @@
+package com.lee.leetcode.pro0051_0075;
+
+/*
+ *
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ *
+ */
+public class Pro_0053_MaximumSubarray {
+
+    public static void main(String[] args) {
+        int result = 0;
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+//        int[] nums = {-2,1};
+//        result = maxSubArray1(nums);
+//        result = maxSubArray2(nums);
+        result = maxSubArray3(nums);
+        System.out.println(result);
+    }
+
+    public static int maxSubArray1(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<=nums.length-1; i++) {
+            int sum = 0;
+            for(int j=i; j<nums.length; j++) {
+                sum += nums[j];
+                if(sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        return maxSum;
+    }
+
+    public static int maxSubArray2(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        int maxSum = nums[0];
+        int subSum = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            if(subSum < 0) {
+                subSum = nums[i];
+            }else {
+                subSum += nums[i];
+            }
+            if(maxSum < subSum) {
+                maxSum = subSum;
+            }
+        }
+        return maxSum;
+    }
+
+    public static int maxSubArray3(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        return maxSubSum(nums, 0, nums.length-1).maxSub;
+    }
+
+    private static SumStatistic maxSubSum(int[] nums, int begin, int end) {
+        if(begin == end) {
+            return new SumStatistic(nums[end], nums[end], nums[end], nums[end]);
+        }
+        int middle = (begin + end) / 2;
+        SumStatistic leftSS = maxSubSum(nums, begin, middle);
+        SumStatistic rightSS = maxSubSum(nums, middle + 1, end);
+        int left = Math.max(leftSS.left, leftSS.total + rightSS.left);
+        int right = Math.max(rightSS.right, rightSS.total + leftSS.right);
+        int total = leftSS.total + rightSS.total;
+        int maxSub = Math.max(leftSS.maxSub, rightSS.maxSub);
+        maxSub = Math.max(maxSub, leftSS.right + rightSS.left);
+        return new SumStatistic(left, right, total, maxSub);
+    }
+
+    private static class SumStatistic {
+        int left;
+        int right;
+        int total;
+        int maxSub;
+        SumStatistic(int left, int right, int total, int maxSub) {
+            this.left = left;
+            this.right = right;
+            this.total = total;
+            this.maxSub = maxSub;
+        }
+    }
+}
---
+++ src/main/java/com/lee/leetcode/pro0051_0075/Pro_0054_SpiralMatrix.java
@@ -0,0 +1,156 @@
+package com.lee.leetcode.pro0051_0075;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ *
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+Input:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ */
+public class Pro_0054_SpiralMatrix {
+
+    public static void main(String[] args) {
+        /*int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };*/
+        int[][] matrix = {
+                {1,2,3,4,5},
+                {6,7,8,9,10},
+                {11,12,13,14,15},
+                {16,17,18,19,20}
+        };
+        List<Integer> result = null;
+//        result = spiralOrder1(matrix);
+        result = spiralOrder2(matrix);
+//        result = spiralOrder3(matrix);
+        System.out.println(result);
+    }
+
+    public static List<Integer> spiralOrder1(int[][] matrix) {
+        if(matrix == null || matrix.length == 0) { return Collections.emptyList(); }
+        if(matrix.length == 1) {
+            int[] array = matrix[0];
+            List<Integer> list = new ArrayList<>(array.length);
+            for(int i=0; i<array.length; i++) {
+                list.add(array[i]);
+            }
+            return list;
+        }
+
+        int m = matrix.length, n = matrix[0].length;
+        if(n == 0) {
+            return Collections.emptyList();
+        }
+        List<Integer> list = new ArrayList<>(m * n);
+        int rb = 0, re = m-1, cb = 0, ce = n-1;
+        int flag = 0;
+        while(rb<=re && cb<=ce) {
+            switch(flag) {
+                case 0: // left to right
+                    for(int i=cb; i<=ce; i++) {
+                        list.add(matrix[rb][i]);
+                    }
+                    rb += 1;
+                    break;
+                case 1: // up to down
+                    for(int i=rb; i<=re; i++) {
+                        list.add(matrix[i][ce]);
+                    }
+                    ce -= 1;
+                    break;
+                case 2: // right to left
+                    for(int i=ce; i>=cb; i--) {
+                        list.add(matrix[re][i]);
+                    }
+                    re -= 1;
+                    break;
+                case 3: // down to up
+                    for(int i=re; i>=rb; i--) {
+                        list.add(matrix[i][cb]);
+                    }
+                    cb += 1;
+                    break;
+                default:    // nop
+                    ;
+            }
+            flag = (flag + 1) % 4;
+        }
+        return list;
+    }
+
+    public static List<Integer> spiralOrder2(int[][] matrix) {
+        if(matrix == null || matrix.length == 0) {
+            return Collections.emptyList();
+        }
+        int m = matrix.length, n = matrix[0].length;
+        if(n == 0) {
+            return Collections.emptyList();
+        }
+        if(m == 1) {
+            int[] array = matrix[0];
+            List<Integer> list = new ArrayList<>(n);
+            for(int i=0; i<array.length; i++) {
+                list.add(array[i]);
+            }
+            return list;
+        }
+
+        int size = m * n;
+        List<Integer> list = new ArrayList<>(m * n);
+        int rb = 0, re = m-1, cb = 0, ce = n-1;
+        int count = 0;
+        while(count < size) {
+            // left to right
+            for(int i=cb; i<=ce; i++) {
+                list.add(matrix[rb][i]);
+                count++;
+            }
+            rb += 1;
+            if(count == size) { break; }
+            // up to down
+            for(int i=rb; i<=re; i++) {
+                list.add(matrix[i][ce]);
+                count++;
+            }
+            ce -= 1;
+            if(count == size) { break; }
+            // right to left
+            for(int i=ce; i>=cb; i--) {
+                list.add(matrix[re][i]);
+                count++;
+            }
+            re -= 1;
+            if(count == size) { break; }
+            // down to up
+            for(int i=re; i>=rb; i--) {
+                list.add(matrix[i][cb]);
+                count++;
+            }
+            cb += 1;
+        }
+        return list;
+    }
+}
