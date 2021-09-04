cc8a14e1c8db8244bd4a3cf668c69255f0b8adc6
halty <halty86@163.com>
2021-09-04 17:48:08 +0800

    add Problem_326 solution

--- src/main/java/com/lee/leetcode/pro0301_0325/Pro_0326_PowerOfThree.java
+++ src/main/java/com/lee/leetcode/pro0301_0325/Pro_0326_PowerOfThree.java
@@ -26,10 +26,43 @@ package com.lee.leetcode.pro0301_0325;
 public class Pro_0326_PowerOfThree {
 
     public static void main(String[] args) {
-
+        int n = 27;
+        // n = 0;
+        // n = 9;
+        // n = 45;
+        // n = -10;
+        System.out.println(isPowerOfThree2(n));
+        /*long s = 3;
+        while(s < Integer.MAX_VALUE) {
+            System.out.println(s);
+            s *= 3;
+        }*/
     }
 
     public static boolean isPowerOfThree(int n) {
+        int s = 3;
+        while (s < n) {
+            s *= 3;
+        }
+        return s == n;
+    }
 
+    public static boolean isPowerOfThree1(int n) {
+        if(n > 1) {
+            if(n % 3 == 0) {
+                return isPowerOfThree1(n /= 3);
+            }else {
+                return false;
+            }
+        }else if(n == 1) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean isPowerOfThree2(int n) {
+        // [3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489, 1162261467]
+        return n >= 3 && 1162261467 % n == 0;
     }
 }
